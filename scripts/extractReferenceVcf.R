085ed4f372a7aff112dada48a124511b2e9f9f42
Caspar Gross <mail@caspar.one>
2018-09-22 11:16:24 +0200

    updatet vcf analysis script

--- scripts/extractReferenceVcf.R
+++ scripts/extractReferenceVcf.R
@@ -8,9 +8,17 @@ ASSEMBLY_MINI <- '/home/dkoppstein/ngschool/src/fantavier/10_survivor/assemblyti
 vcm <- read.vcf(ASSEMBLY_MINI)
 vcr <- read.vcf(REFERENCE)
 
-
+# Convert to data.table
 dtm <- as.data.table(vcm$vcf)
 dtr <- as.data.table(vcr$vcf)
 
+# Extract Info fields
+dtm_info <- as.data.table(t(strsplit(dtm$INFO, ";")))
 
+dtm_info <- dtm[1:nrow(dtm), tstrsplit(INFO, ";"),]
+dtr_info <- dtr[1:nrow(dtr), tstrsplit(INFO, ";"),]
+
+dtm_info_split <- dtm_info[, .(SVTYPE = tstrsplit(V2, '=' )[2], CHR2 = tstrsplit(V4, "=")[2], END = tstrsplit(V5, "="), SVLEN = tstrsplit(V6, "=")),]
+
+dtr_info_split <- dtm_info[, .(SVTYPE = tstrsplit(V2, '=' )[2], CHR2 = tstrsplit(V4, "=")[2], END = tstrsplit(V5, "="), SVLEN = tstrsplit(V6, "=")),]
 
